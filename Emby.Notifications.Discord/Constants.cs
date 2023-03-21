0bbc6a198414cd9f590f72e7a94cb3caa9eb17c0
Luke Pulverenti <3607463+LukePulverenti@users.noreply.github.com>
2023-03-21 18:58:36 -0400

    removed dead code

--- Emby.Notifications.Discord/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Emby.Notifications.Discord
-{
-    class Constants
-    {
-        public static readonly int RecheckIntervalMS = 10000;
-        public static readonly int MaxRetriesBeforeFallback = 10;
-        public static readonly int MessageQueueSendInterval = 1000;
-    }
-}
